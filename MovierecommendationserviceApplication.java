8b60146b5e2c542bca1b658cefa12b0298feef44
crotert <crotert@gmail.com>
2019-10-27 20:42:18 -0500

    MoverecommendationserviceApplication.java

---
+++ MovierecommendationserviceApplication.java
@@ -0,0 +1,21 @@
+package cdm.depaul.edu.se480.homework3.movierecommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class MovierecommendationserviceApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(MovierecommendationserviceApplication.class, args);
+	}
+
+}
