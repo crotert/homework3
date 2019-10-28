bf2b93444a0768ce67a727aa84b61c155e7ff5ba
crotert <crotert@gmail.com>
2019-10-27 20:19:07 -0500

    Added UserServiceApplication.java

---
+++ UserserviceApplication.java
@@ -0,0 +1,13 @@
+package cdm.depaul.edu.se480.homework3.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+}
